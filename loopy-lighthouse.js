a2025edb8f7f2b58e02e231f0dcd1a38e23eb5d2
MysticsGitHub <bradyjames97@hotmail.com>
2023-04-01 21:35:30 +0000

    updated to be a function

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,11 +1,14 @@
-for (let num = 100; num <= 200; num++){
-  if(num % 3 === 0 && num % 4 === 0){
-    console.log("LoopyLighthouse");
-  } else if(num % 3 === 0){
-    console.log("Loopy");
-  } else if(num % 4 === 0){
-    console.log("Lighthouse");
-  } else {
-    console.log(num)
+function loopyLighthouse(range, multiples, words){
+  for (let num = range[0]; num <= range[1]; num++){
+    if(num % multiples[0] === 0 && num % multiples[1] === 0){
+      console.log(words[0] + words[1]);
+    } else if(num % multiples[0] === 0){
+      console.log(words[0]);
+    } else if(num % multiples[1] === 0){
+      console.log(words[1]);
+    } else {
+      console.log(num);
+    }
   }
 }
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
